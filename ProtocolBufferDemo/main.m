b0e5126cb8323d5855b8bba0157ce5a00086b9f9
frank <fanxiushan@gmail.com>
2017-12-04 19:14:13 +0800

    init
    Signed-off-by: frank <fanxiushan@gmail.com>

--- ProtocolBufferDemo/main.m
+++ ProtocolBufferDemo/main.m
@@ -7,11 +7,20 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "Person.pbobjc.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        NSLog(@"Hello, World!");
+        Person *personRoot = [[Person alloc] init];
+        personRoot.name = @"hello";
+        NSData *personData = [personRoot delimitedData];
+        NSLog(@"length %lu",(unsigned long)personData.length);
+        //二进制数据反序列化为对象
+        GPBCodedInputStream *inputStream = [GPBCodedInputStream streamWithData:personData];
+        NSError *error;
+        Person *per = [Person parseDelimitedFromCodedInputStream:inputStream extensionRegistry:nil error:&error];
+        NSLog(@"person = %@",per.name);
     }
     return 0;
 }
